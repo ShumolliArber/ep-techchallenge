e84477a1b2aee78b985b9ff3b11bd586daf52a54
Arber Shumolli <arbi_sh@hotmail.com>
2025-03-01 22:43:25 +0100

    feature: show client bookings by newest first

--- app/Client.php
+++ app/Client.php
@@ -24,7 +24,7 @@ class Client extends Model
 
     public function bookings(): HasMany
     {
-        return $this->hasMany(Booking::class);
+        return $this->hasMany(Booking::class)->latest('start');
     }
 
     public function user(): BelongsTo
